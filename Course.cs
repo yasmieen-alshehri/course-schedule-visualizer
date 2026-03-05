102c7f1ada956f5b19e83e02c6165e4842a09392
Yasmeen Alshehri <s202271660@kfupm.edu.sa>
2026-03-05 22:12:26 +0300

    Add files via upload

---
+++ Course.cs
@@ -0,0 +1,20 @@
+﻿namespace xAcademics
+{
+    public class Course
+    {
+       
+
+        public string Code { get; }
+        public string Title { get; }
+        public string Dept { get; }
+
+        public Course(string code, string title, string dept)
+        {
+            Code = code ?? "";
+            Title = title ?? "";
+            Dept = dept ?? "";
+        }
+
+        public override string ToString() => Code;
+    }
+}
